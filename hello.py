59732273ea5cabe4a4be7cab96c78086fcf22b1d
Colin Wilder <colinwilder@gmail.com>
2025-09-05 14:04:52 -0400

    new test

---
+++ hello.py
@@ -0,0 +1,12 @@
+from datetime import date
+
+def say_hello(name):
+    print("Hello, " + name)
+
+say_hello("VS Code")
+
+def say_day_of_week(date):
+    days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    return days[date.weekday()]
+
+print(say_day_of_week(date.today()))
